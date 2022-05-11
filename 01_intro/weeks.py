3783a8c6f448d34be1324ec4758ceb3249ff78e1
Kubases <mokwajak@gmail.com>
2022-05-11 14:16:07 +0200

    first commit

---
+++ 01_intro/weeks.py
@@ -0,0 +1,13 @@
+weeks = 7
+days = 7
+hours_in_day = 24
+minutes_in_hour = 60
+a = weeks * days * hours_in_day * minutes_in_hour
+print(a)
+print()
+
+
+course_time = 75
+my_time = 12
+b = course_time / my_time
+print(b)
